51fb190bebfc056e4aafbef269e7b498614114ed
guochao <463561248@qq.com>
2022-02-22 18:54:48 +0800

    arco-cli: initialize @dangojs/saber-button-test

---
+++ src/demo/basic.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import TooltipButton from '@dangojs/saber-button-test';
+
+export default () => {
+  return <TooltipButton title="tooltip title">Demo Basic</TooltipButton>;
+};
