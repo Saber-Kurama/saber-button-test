69f3a9cbb760e496a36ae3ce35a0d3da3d755952
guochao <463561248@qq.com>
2022-02-22 19:31:06 +0800

    first commit

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,8 +1,8 @@
 /**
  * @file
- * @title 组件名
- * @memberOf 组件类型，例如：数据输入
- * @description 描述你的组件。
+ * @title saber-button-test
+ * @memberOf 数据输入
+ * @description 测试物料。
  * @author 物料作者
  * @package @dangojs/saber-button-test
  */
